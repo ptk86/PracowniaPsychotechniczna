0398bac4df9775902cba5426e66bf48b7b5579e4
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-15 01:32:09 +0200

    Add connection to database via ApplicationContext

---
+++ PracowniaPsychotechniczna/Dal/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PracowniaPsychotechniczna.Model;
+
+namespace PracowniaPsychotechniczna.Dal
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Badanie> Badanies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<PracowniaPsychotechniczna.Model.Psycholog> Psycholog { get; set; }
+    }
+}
