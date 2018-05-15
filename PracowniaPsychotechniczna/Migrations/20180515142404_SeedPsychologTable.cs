384854543162488a2368bce222af382f2b173944
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-15 16:35:36 +0200

    Add: migration that seeds Psycholog table.

---
+++ PracowniaPsychotechniczna/Migrations/20180515142404_SeedPsychologTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PracowniaPsychotechniczna.Migrations
+{
+    public partial class SeedPsychologTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                SET IDENTITY_INSERT PracowniaPsychologiczna.dbo.Psycholog ON;
+                INSERT INTO PracowniaPsychologiczna.dbo.Psycholog (Id, Imie, Nazwisko) VALUES (1, 'Łukasz', 'Koziarski');
+                INSERT INTO PracowniaPsychologiczna.dbo.Psycholog (Id, Imie, Nazwisko) VALUES (2, 'Amadeusz', 'Nowak');
+                INSERT INTO PracowniaPsychologiczna.dbo.Psycholog (Id, Imie, Nazwisko) VALUES (3, 'Piotr', 'Kiliński');
+                SET IDENTITY_INSERT PracowniaPsychologiczna.dbo.Psycholog OFF;
+
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM PracowniaPsychologiczna.dbo.Psycholog;
+            ");
+        }
+    }
+}
