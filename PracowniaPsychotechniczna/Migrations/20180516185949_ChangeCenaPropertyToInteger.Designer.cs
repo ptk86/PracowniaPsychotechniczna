4839e6e0dd910c7689bd67cdb96757381a77443a
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-16 21:42:47 +0200

    Add: migration that change Cena column to be of int type

--- PracowniaPsychotechniczna/Migrations/20180516185949_ChangeCenaPropertyToInteger.Designer.cs
+++ PracowniaPsychotechniczna/Migrations/20180516185949_ChangeCenaPropertyToInteger.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PracowniaPsychotechniczna.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20180516185949_ChangeCenaPropertyToInteger")]
+    partial class ChangeCenaPropertyToInteger
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PracowniaPsychotechniczna/Migrations/20180516185949_ChangeCenaPropertyToInteger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PracowniaPsychotechniczna.Migrations
+{
+    public partial class ChangeCenaPropertyToInteger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Cena",
+                table: "TypBadania",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cena",
+                table: "TypBadania",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
