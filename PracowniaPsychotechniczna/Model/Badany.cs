f4bb999b7ef9d8448f0de9da97fc12140e33833e
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-20 23:55:08 +0200

    Modify: exchange attributes to FluentValidations for Badany.

--- PracowniaPsychotechniczna/Model/Badany.cs
+++ PracowniaPsychotechniczna/Model/Badany.cs
@@ -11,22 +11,11 @@ namespace PracowniaPsychotechniczna.Model
     public class Badany
     {
         public int Id { get; set; }
-
         [DisplayName("Imię")]
-        [Required(ErrorMessage = "Imię jest wymagane!")]
         public string Imie { get; set; }
-
-        [Required(ErrorMessage = "Nazwisko jest wymagane!")]
         public string Nazwisko { get; set; }
-
-        [Required(ErrorMessage = "Pesel jest wymagany!")]
-        [RegularExpression(@"^\d{11}$", ErrorMessage = "PESEL musi być ciągiem 11 cyfr")]
         public string Pesel { get; set; }
-
-        [Required]
         public string Adres { get; set; }
-
-
     }
 
 }
--- PracowniaPsychotechniczna/Model/BadanyValidatior.cs
+++ PracowniaPsychotechniczna/Model/BadanyValidatior.cs
@@ -13,12 +13,25 @@ namespace PracowniaPsychotechniczna.Model
         {
             _context = context;
 
-            RuleFor(b => b.Pesel).Must(IsPeselValid).WithMessage("Nieprawidłowy PESEL!");
+            RuleFor(b => b.Imie).NotEmpty();
+            RuleFor(b => b.Nazwisko).NotEmpty();
+            RuleFor(b => b.Pesel).NotEmpty();
+            RuleFor(b => b.Pesel).Matches(@"^\d{11}$");
+            RuleFor(b => b.Pesel).Must(IsPeselChecksumValid).WithMessage("Nieprawidłowy PESEL!");
+            RuleFor(b => b.Pesel).Must( (b, p) => IsUnique(p, b.Id)).WithMessage("Ten pesel został już zarejestrowany!");
+            RuleFor(b => b.Adres).NotEmpty();
         }
 
-        public bool IsPeselValid(string pesel)
+        public bool IsPeselChecksumValid(string pesel)
         {
-            return ValidationHelper.ValidateChecksum(pesel, new[] { 9, 7, 3, 1, 9, 7, 3, 1, 9, 7 });
+            return ValidationHelper.ValidateModuloChecksum(pesel, new[] { 9, 7, 3, 1, 9, 7, 3, 1, 9, 7 });
+        }
+
+        public bool IsUnique(string pesel, int id)
+        {
+            return _context.Badany
+                            .Where(b => b.Id != id)
+                            .All(b => b.Pesel != pesel);
         }
     }
 
