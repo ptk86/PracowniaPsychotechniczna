b170d819f8415dccd39c3f1611205a25e2d0ee59
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-21 00:04:09 +0200

    Modify: replace validation attributes with fluent validation rules.

--- PracowniaPsychotechniczna/Model/Psycholog.cs
+++ PracowniaPsychotechniczna/Model/Psycholog.cs
@@ -1,17 +1,14 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using PracowniaPsychotechniczna.Dal;
 
 namespace PracowniaPsychotechniczna.Model
 {
     public class Psycholog
     {
         public int Id { get; set; }
-
         [DisplayName("Imię")]
-        [Required(ErrorMessage = "Imię jest wymagane!")]
         public string Imie { get; set; }
-
-        [Required(ErrorMessage = "Nazwisko jest wymagane!")]
         public string Nazwisko { get; set; }
     }
 }
---
+++ PracowniaPsychotechniczna/Validator/PsychologValidatior.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PracowniaPsychotechniczna.Model;
+
+namespace PracowniaPsychotechniczna.Validator
+{
+    public class PsychologValidatior : AbstractValidator<Psycholog>
+    {
+        public PsychologValidatior()
+        {
+            RuleFor(b => b.Imie).NotEmpty();
+            RuleFor(b => b.Nazwisko).NotEmpty();
+        }
+    }
+}
