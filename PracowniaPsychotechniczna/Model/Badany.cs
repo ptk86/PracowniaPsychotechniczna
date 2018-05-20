6b8642798c0440ca3589049ac5ff84491cd229d6
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-20 10:31:44 +0200

    Modify add validation attributes.

--- PracowniaPsychotechniczna/Model/Badany.cs
+++ PracowniaPsychotechniczna/Model/Badany.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace PracowniaPsychotechniczna.Model
 {
@@ -9,16 +12,20 @@ namespace PracowniaPsychotechniczna.Model
     {
         public int Id { get; set; }
 
-        [Required]
+        [DisplayName("Imię")]
+        [Required(ErrorMessage = "Imię jest wymagane!")]
         public string Imie { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Nazwisko jest wymagane!")]
         public string Nazwisko { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pesel jest wymagany!")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "PESEL musi być ciągiem 11 cyfr")]
         public string Pesel { get; set; }
 
         [Required]
         public string Adres { get; set; }
+
+
     }
 }
