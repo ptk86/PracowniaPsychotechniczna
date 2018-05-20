cb2a092bc1e43d588aec75796edc3acb9cab1c9c
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-20 15:00:00 +0200

    Add Validation to deal with checksum validation Add FirmaValidatior to deal with complex Nip validation

---
+++ PracowniaPsychotechniczna/Model/FrimaValidatior.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using PracowniaPsychotechniczna.Dal;
+
+namespace PracowniaPsychotechniczna.Model
+{
+    public class FrimaValidatior : AbstractValidator<Firma>
+    {
+        private readonly ApplicationContext _context;
+
+        public FrimaValidatior(ApplicationContext context)
+        {
+            _context = context;
+
+            RuleFor(b => b.Nip).Must(IsNipValid).WithMessage("Nieprawidłowy PESEL!");
+        }
+
+        public bool IsNipValid(string nip)
+        {
+            return ValidationHelper.ValidateChecksum(nip, new [] { 6, 5, 7, 2, 3, 4, 5, 6, 7 });
+        }
+
+
+    }
+}
---
+++ PracowniaPsychotechniczna/Model/ValidationHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace PracowniaPsychotechniczna.Model
+{
+    public class ValidationHelper
+    {
+        public static bool ValidateChecksum(string toValidate, int[] factors)
+        {
+            var digits = toValidate.Select(p => int.Parse(p.ToString())).ToList();
+            var sum = digits.Zip(factors, (pd, f) => pd * f).Sum();
+            var modulo = sum % 10;
+            return modulo == digits.Last();
+        }
+    }
+}
