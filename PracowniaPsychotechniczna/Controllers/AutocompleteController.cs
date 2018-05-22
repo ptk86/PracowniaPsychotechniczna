4d667353f283253bbf20603e07b3ea83f90ad33a
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-22 02:20:17 +0200

    Add: autocomplete controller.

---
+++ PracowniaPsychotechniczna/Controllers/AutocompleteController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PracowniaPsychotechniczna.Dal;
+
+namespace PracowniaPsychotechniczna.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/autocomplete")]
+    public class AutocompleteController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public AutocompleteController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("badani")]
+        public IActionResult GetBadani(string searchTerm)
+        {
+            var filtered = _context.Badany
+                                .Where(b => b.Pesel.StartsWith(searchTerm) ||
+                                            b.Imie.StartsWith(searchTerm) ||
+                                            b.Nazwisko.StartsWith(searchTerm) );
+
+            return Ok(filtered);
+        }
+
+        [HttpGet("firmy")]
+        public IActionResult GetFirmy(string searchTerm)
+        {
+            return Ok(_context.Firma
+                .Where(f => f.Nazwa.StartsWith(searchTerm) ||
+                            f.Nip.StartsWith(searchTerm)));
+        }
+    }
+}
