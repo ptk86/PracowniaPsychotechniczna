3c03a1af708273c6abe13266ca2d759e7aef0d95
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-22 17:02:31 +0200

    Add: CreateBadanieValidator

---
+++ PracowniaPsychotechniczna/Validator/CreateBadanieValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using FluentValidation;
+using PracowniaPsychotechniczna.Dal;
+using PracowniaPsychotechniczna.Pages.Badanie;
+
+namespace PracowniaPsychotechniczna.Validator
+{
+    public class CreateBadanieValidator : AbstractValidator<CreateBadanie>
+    {
+        private readonly ApplicationContext _context;
+
+        public CreateBadanieValidator(ApplicationContext context)
+        {
+            _context = context;
+
+            RuleFor(cb => cb.BadanyId).Must(id => context.Badany.Any(b => b.Id == id))
+                .WithMessage("Brak Badanego w bazie danych!");
+            RuleFor(cb => cb.FirmaId).Must(id => context.Firma.Any(b => b.Id == id))
+                .WithMessage("Brak Firmy w bazie danych!");
+        }
+        
+    }
+}
