336af96a9654245f3ae24c7869230928d263aeba
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-05-22 21:31:08 +0200

    Modify: uncomment wrongly commented rule Modify: DisplayNames for BadanieListItem

--- PracowniaPsychotechniczna/Pages/Badanie/BadanieIndexListItem.cs
+++ PracowniaPsychotechniczna/Pages/Badanie/BadanieIndexListItem.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace PracowniaPsychotechniczna.Pages.Badanie
 {
     public class BadanieIndexListItem
     {
         public int Id { get; set; }
+        [DisplayName("Typ Badania")]
         public string TypBadania { get; set; }
+        [DisplayName("Imię")]
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
         public string Frima { get; set; }
-        public bool CzyPlatnoscWlasna { get; set; }
+        [DisplayName("Data Badania")]
         public DateTime DataBadania { get; set; }
     }
 }
